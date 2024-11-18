458f3d5bec9667dace1ade7d34f1e7fa68d32a37
Yuri <yuri.conte@hotmail.com>
2024-11-17 23:45:34 -0300

    inclusao de items

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: 'http://localhost:8080'
-    //apiUrl: 'http://teste-env.eba-wpwuiwpj.sa-east-1.elasticbeanstalk.com'
+    //apiUrl: 'http://localhost:8080'
+    apiUrl: 'http://teste-env.eba-wpwuiwpj.sa-east-1.elasticbeanstalk.com'
 };
